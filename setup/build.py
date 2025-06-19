26df3bbaff2252dfdf0dd9c1a70020ff9215a9bb
BladerX11 <BladerX11@proton.me>
2025-06-20 00:57:08 +0800

    chore: change build to nuitka

--- setup/build.py
+++ setup/build.py
@@ -1,15 +1,31 @@
-import PyInstaller.__main__
+import os
+import subprocess
+import sys
 
-PyInstaller.__main__.run(
-    [
-        "src/main.py",
-        "--onefile",
-        "--windowed",
-        "--collect-submodules",
-        "azure.cognitiveservices.speech",
-        "--add-data",
-        "resources:resources",
-        "--name",
-        "VocalScript",
-    ]
-)
+version = "0.1"
+command = [
+    sys.executable,
+    "-m",
+    "nuitka",
+    "--mode=app",
+    f"--product-version={version}",
+    "--windows-console-mode=disable",
+    f"--macos-app-version={version}",
+    "--enable-plugin=pyside6",
+    "--output-dir=dist",
+    "--output-filename=vocalscript",
+    "--include-package=azure.cognitiveservices.speech",
+    "--include-data-dir=resources=resources",
+    "src/main.py",
+]
+
+if sys.platform == "win32" and os.environ.get("CI") == "true":
+    command.append("--assume-yes-for-downloads")
+
+try:
+    _ = subprocess.run(
+        command,
+        check=True,
+    )
+except subprocess.CalledProcessError as e:
+    sys.exit(e.returncode)
