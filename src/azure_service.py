83389524e05cd22956734e6b468c172bde5552dc
BladerX11 <BladerX11@proton.me>
2025-06-25 16:13:21 +0800

    feat: save multiple audio files

--- src/azure_service.py
+++ src/azure_service.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 import logging
+from time import sleep
 
 from azure.cognitiveservices.speech import (
+    AudioDataStream,
     CancellationReason,
     SpeechConfig,
     SpeechSynthesisEventArgs,
@@ -10,6 +13,7 @@ from azure.cognitiveservices.speech.audio import AudioOutputConfig
 from azure.cognitiveservices.speech.diagnostics.logging import EventLogger
 
 from settings import settings
+from utils import get_folder
 
 speech_config = SpeechConfig(
     subscription=str(settings.value("key", " ")),
@@ -18,10 +22,7 @@ speech_config = SpeechConfig(
 speech_config.speech_synthesis_voice_name = str(
     settings.value("voice_name", "en-US-AlloyTurboMultilingualNeural")
 )
-audio_config = AudioOutputConfig(filename="output.wav")
-speech_synthesizer = SpeechSynthesizer(
-    speech_config=speech_config, audio_config=audio_config
-)
+speech_synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=None)
 logger = logging.getLogger(__name__)
 
 
@@ -32,13 +33,29 @@ def log(msg: str):
         logger.error(msg)
 
 
+def synthesis_completed(event: SpeechSynthesisEventArgs):
+    stream = AudioDataStream(event.result)
+    folder = get_folder() / "saved"
+
+    if not folder.exists():
+        folder.mkdir()
+
+    file = str(folder / (datetime.now().isoformat() + ".wav"))
+    stream.save_to_wav_file(file)
+
+
 def synthesis_canceled(event: SpeechSynthesisEventArgs):
     cancellation_details = event.result.cancellation_details
     if cancellation_details.reason == CancellationReason.Error:
         logger.error(cancellation_details.error_details)
-    EventLogger.set_callback()
 
 
 EventLogger.set_callback(log)
 
+speech_synthesizer.synthesis_completed.connect(
+    lambda e: AudioDataStream(e.result).save_to_wav_file(
+        str(get_folder() / "saved" / (datetime.now().isoformat() + ".wav"))
+    )
+)
 speech_synthesizer.synthesis_canceled.connect(synthesis_canceled)
+speech_synthesizer.synthesis_completed.connect(synthesis_completed)
