dabedbbb55901fe4f528d75f8c39b5a84d084aa4
BladerX11 <BladerX11@proton.me>
2025-06-25 14:15:15 +0800

    feat: open settings dialog from menubar

--- src/widgets/main_window.py
+++ src/widgets/main_window.py
@@ -11,6 +11,7 @@ from PySide6.QtWidgets import (
 )
 
 from azure_service import speech_synthesizer
+from widgets.settings import Settings
 from widgets.status_bar import StatusBar
 
 
@@ -23,17 +24,19 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(QWidget(self))
         self.setStatusBar(StatusBar(self))
 
-        basedir = Path(__file__).parent.parent
-        if "__compiled__" not in globals():
-            basedir = basedir.parent
+        _ = (
+            self.menuBar()
+            .addAction("&Settings")
+            .triggered.connect(lambda: Settings(self).open())
+        )
 
         self.input_field: QPlainTextEdit = QPlainTextEdit(self)
 
         submit_button = QPushButton("Save", self)
-        submit_button.setIcon(QIcon(str(basedir / "resources" / "download.svg")))
+        submit_button.setIcon(QIcon(self.__get_resource("download.svg")))
         _ = submit_button.clicked.connect(self.__on_submit)
 
-        main_layout: QVBoxLayout = QVBoxLayout()
+        main_layout: QVBoxLayout = QVBoxLayout(self.centralWidget())
         main_layout.addWidget(self.input_field)
         main_layout.addWidget(submit_button)
         self.centralWidget().setLayout(main_layout)
@@ -42,3 +45,11 @@ class MainWindow(QMainWindow):
         text = self.input_field.toPlainText().strip()
         self.logger.info(f"Synthesising started for: {text}")
         _ = speech_synthesizer.speak_text_async(text)
+
+    def __get_resource(self, name: str):
+        basedir = Path(__file__).parent.parent
+
+        if "__compiled__" not in globals():
+            basedir = basedir.parent
+
+        return str(basedir / "resources" / name)
