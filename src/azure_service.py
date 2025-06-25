23a2032aeb84b5dabf111f10b05b9de3170e135c
BladerX11 <BladerX11@proton.me>
2025-06-25 23:31:52 +0800

    feat: Improve logging

--- src/azure_service.py
+++ src/azure_service.py
@@ -1,11 +1,12 @@
-from datetime import datetime
 import logging
+from datetime import datetime
 
 from azure.cognitiveservices.speech import (
     AudioDataStream,
     CancellationReason,
     SpeechConfig,
     SpeechSynthesisEventArgs,
+    SpeechSynthesisResult,
     SpeechSynthesizer,
 )
 from azure.cognitiveservices.speech.diagnostics.logging import EventLogger
@@ -13,41 +14,65 @@ from azure.cognitiveservices.speech.diagnostics.logging import EventLogger
 from settings import settings
 from utils import from_executable
 
-speech_config = SpeechConfig(
+_logger = logging.getLogger(__name__)
+_speech_config = SpeechConfig(
     subscription=str(settings.value("key", " ")),
     endpoint=str(settings.value("endpoint", " ")),
 )
-speech_config.speech_synthesis_voice_name = str(
+_speech_config.speech_synthesis_voice_name = str(
     settings.value("voice_name", "en-US-AlloyTurboMultilingualNeural")
 )
-speech_synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=None)
-logger = logging.getLogger(__name__)
+speech_synthesizer = SpeechSynthesizer(speech_config=_speech_config, audio_config=None)
 
 
-def log(msg: str):
+def _log(msg: str):
     if "INFO" in msg:
-        logger.info(msg)
+        _logger.info(msg)
     elif "ERROR" in msg:
-        logger.error(msg)
+        _logger.error(msg)
 
 
-def synthesis_completed(event: SpeechSynthesisEventArgs):
-    stream = AudioDataStream(event.result)
-    folder = from_executable("saved")
-
-    if not folder.exists():
-        folder.mkdir()
-
-    file = str(folder / (datetime.now().isoformat() + ".wav"))
-    stream.save_to_wav_file(file)
-
-
-def synthesis_canceled(event: SpeechSynthesisEventArgs):
+def _synthesis_canceled(event: SpeechSynthesisEventArgs):
     cancellation_details = event.result.cancellation_details
     if cancellation_details.reason == CancellationReason.Error:
-        logger.error(cancellation_details.error_details)
+        _logger.error(
+            "Synthesis failed. Error details: %s", cancellation_details.error_details
+        )
+    else:
+        _logger.info("Synthesis canceled")
 
 
-EventLogger.set_callback(log)
-speech_synthesizer.synthesis_canceled.connect(synthesis_canceled)
-speech_synthesizer.synthesis_completed.connect(synthesis_completed)
+EventLogger.set_callback(_log)
+speech_synthesizer.synthesis_completed.connect(
+    lambda _: _logger.info("Synthesis completed")
+)
+speech_synthesizer.synthesis_canceled.connect(_synthesis_canceled)
+
+
+def speak_text_async(text: str):
+    _logger.info("Synthesising. Text: %s", text)
+    speech_synthesizer.speak_text_async(text)
+
+
+def save_to_wav_file(
+    result: SpeechSynthesisResult,
+):
+    stream = AudioDataStream(result)
+    folder = from_executable("saved")
+    file = folder / (datetime.now().isoformat() + ".wav")
+    _logger.info("Saving. File name: %s", file.name)
+
+    try:
+        folder.mkdir(exist_ok=True)
+        stream.save_to_wav_file(str(file))
+    except FileExistsError as e:
+        msg = f"Saving failed. File {folder.name} already exists. Please move it and try again."
+        _logger.error("Saving failed.", exc_info=e)
+    except Exception as e:
+        msg = "Saving failed. Check log."
+        _logger.error("Saving failed.", exc_info=e)
+    else:
+        msg = "Saving completed."
+        _logger.info("Saving completed")
+
+    return msg
--- src/widgets/status_bar.py
+++ src/widgets/status_bar.py
@@ -2,7 +2,7 @@ from azure.cognitiveservices.speech import SpeechSynthesisEventArgs
 from azure.cognitiveservices.speech.speech import CancellationReason
 from PySide6.QtWidgets import QStatusBar
 
-from azure_service import speech_synthesizer
+from azure_service import save_to_wav_file, speech_synthesizer
 
 
 class StatusBar(QStatusBar):
@@ -10,19 +10,21 @@ class StatusBar(QStatusBar):
         super().__init__(parent)
 
         speech_synthesizer.synthesis_started.connect(
-            lambda _: self.showMessage("Synthesising")
+            lambda _: self.showMessage("Synthesising.")
         )
-        speech_synthesizer.synthesis_completed.connect(
-            lambda _: self.showMessage("Completed")
-        )
-        speech_synthesizer.synthesis_canceled.connect(self.synthesis_canceled)
+        speech_synthesizer.synthesis_completed.connect(self._synthesis_completed)
+        speech_synthesizer.synthesis_canceled.connect(self._synthesis_canceled)
 
-    def synthesis_canceled(self, event: SpeechSynthesisEventArgs):
+    def _synthesis_completed(self, event: SpeechSynthesisEventArgs):
+        self.showMessage("Saving.")
+        self.showMessage(save_to_wav_file(event.result))
+
+    def _synthesis_canceled(self, event: SpeechSynthesisEventArgs):
         cancellation_details = event.result.cancellation_details
 
         if cancellation_details.reason == CancellationReason.Error:
-            msg = "Error. Check log"
+            msg = "Synthesis failed. Check log."
         else:
-            msg = "Canceled"
+            msg = "Synthesis canceled."
 
         self.showMessage(msg)
