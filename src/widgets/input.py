c3ed2d42c4980eeae6840020fd6624b2bba533bc
BladerX11 <BladerX11@proton.me>
2025-07-07 16:26:20 +0800

    feat: add play button to input

--- src/widgets/input.py
+++ src/widgets/input.py
@@ -47,15 +47,19 @@ class Input(QWidget):
             )
         )
 
-        submit_button = QPushButton("Save", self)
-        submit_button.setIcon(QIcon(self._get_resource("download.svg")))
-        _ = submit_button.clicked.connect(self._on_submit)
+        play_button = QPushButton("Play", self)
+        _ = play_button.clicked.connect(self._on_play)
+
+        save_button = QPushButton("Save", self)
+        save_button.setIcon(QIcon(self._get_resource("download.svg")))
+        _ = save_button.clicked.connect(self._on_save)
 
         bottom_layout = QHBoxLayout()
         bottom_layout.addWidget(character_count)
         bottom_layout.addStretch()
         bottom_layout.addWidget(self.use_ssml)
-        bottom_layout.addWidget(submit_button)
+        bottom_layout.addWidget(play_button)
+        bottom_layout.addWidget(save_button)
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.input_field)
@@ -78,9 +82,10 @@ class Input(QWidget):
 
         return str(basedir / "resources" / name)
 
-    def _on_submit(self):
+    def _on_save(self):
         """Handle the submit button click by synthesizing speech or emitting error status."""
         input = self.input_field.toPlainText().strip()
+
         try:
             if self.use_ssml.isChecked():
                 TtsService.get_service().save_ssml_to_file_async(
@@ -90,5 +95,17 @@ class Input(QWidget):
                 TtsService.get_service().save_text_to_file_async(
                     input, self.status.emit
                 )
+        except MissingInformationError:
+            self.status.emit("Service information required to generate audio.")
+
+    def _on_play(self):
+        """Handle the play button click by synthesizing speech or emitting error status."""
+        input = self.input_field.toPlainText().strip()
+
+        try:
+            if self.use_ssml.isChecked():
+                TtsService.get_service().play_ssml_async(input, self.status.emit)
+            else:
+                TtsService.get_service().play_text_async(input, self.status.emit)
         except MissingInformationError:
             self.status.emit("Service information required to generate audio.")
