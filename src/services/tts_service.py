163b08b0174b40a791d94be2ff9035595deb3d83
BladerX11 <BladerX11@proton.me>
2025-07-10 14:46:41 +0800

    refactor: move save error checking to service class

--- src/services/tts_service.py
+++ src/services/tts_service.py
@@ -1,27 +1,31 @@
-from abc import ABC, abstractmethod
-from enum import Enum
 import importlib
 import logging
-from typing import Callable
+from abc import ABC, abstractmethod
+from datetime import datetime
+from enum import Enum
+from pathlib import Path
+from typing import Callable, Generic, TypeVar
 
 from settings import settings
+from utils import from_data_dir
 
 _logger = logging.getLogger(__name__)
+T = TypeVar("T")
 
 
 class Services(Enum):
     AZURE = "azure"
 
 
-class TtsService(ABC):
-    _current_service: "TtsService | None" = None
+class TtsService(Generic[T], ABC):
+    _current_service: "TtsService[object] | None" = None
 
     @abstractmethod
     def __init__(self, *args: str):
         pass
 
     @classmethod
-    def get_service(cls) -> "TtsService":
+    def get_service(cls) -> "TtsService[object]":
         """Returns existing instance or creates one based on saved type from settings."""
         if cls._current_service is None:
             try:
@@ -37,7 +41,7 @@ class TtsService(ABC):
         return cls._current_service
 
     @classmethod
-    def _get_service_class(cls, service: Services) -> type["TtsService"]:
+    def _get_service_class(cls, service: Services) -> type["TtsService[object]"]:
         if service == Services.AZURE:
             return getattr(importlib.import_module("services.azure"), "Azure")
 
@@ -66,6 +70,63 @@ class TtsService(ABC):
     def setting_fields(cls) -> list[str]:
         """Returns the setting fields for the TTS service."""
         pass
+
+    @classmethod
+    @abstractmethod
+    def _save_implementation(cls, file: Path, data: T):
+        """Saves the audio to the file
+
+        Args:
+        file (Path): The file path where the audio will be saved.
+        data (T): The audio data to be saved.
+
+        Raises:
+        FileExistsError: If there is a file where the save directory or file is.
+        IsADirectoryError If there is a directory where the save file is.
+        PermissionError: If the save directory or file cannot be created due to lack of permissions.
+        OSError: If there is an error creating the save directory.
+        """
+        pass
+
+    @classmethod
+    def save_audio(cls, data: T) -> str:
+        """Creates save directory and saves audio to a file, handling its errors.
+
+        Args:
+        data (T): The audio data to be saved.
+
+        Returns:
+        msg (str): A message indicating the result of the save operation.
+        """
+        save_dir = "saved"
+        file = None
+        try:
+            folder = from_data_dir(save_dir)
+            _logger.info("Creating save directory. Directory: %s", save_dir)
+            folder.mkdir(exist_ok=True)
+            file = folder / (datetime.now().strftime("%Y%m%d_%H%M%S%f")[:-3] + ".wav")
+            cls._save_implementation(file, data)
+        except FileExistsError as e:
+            _logger.error("Saving failed. Error: %s", e.strerror, exc_info=e)
+            target = f"File {file.name}" if file else f"Folder {save_dir}"
+            msg = (
+                f"Saving failed. {target} already exists. Please move it and try again."
+            )
+        except IsADirectoryError as e:
+            _logger.error("Saving failed. Error: %s", e.strerror, exc_info=e)
+            msg = f"Saving failed. File {file} already exists. Please try again."
+        except PermissionError as e:
+            _logger.error("Saving failed. Error: %s", e.strerror, exc_info=e)
+            target = f"File {file.name}" if file else f"Folder {save_dir}"
+            msg = f"Saving failed. Insufficient permissions for {target}."
+        except OSError as e:
+            _logger.error("Saving failed. Error: %s", e.strerror, exc_info=e)
+            msg = "Saving failed. Filesystem error."
+        else:
+            _logger.info("Saving completed")
+            msg = "Saving completed."
+
+        return msg
 
     @property
     @abstractmethod
